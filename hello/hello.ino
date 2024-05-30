88ca5112310b962b41d191b1fc6714ebd5827bfb
Crawford Lieu <110504159+DefNotCrawf@users.noreply.github.com>
2024-05-31 09:01:59 +1000

    update

--- hello/hello.ino
+++ hello/hello.ino
@@ -1,24 +1,51 @@
-#define LED_PIN 13
+#include <Arduino.h>
+#define boardLED 13
 
-class Led 
-{
-private:
-  byte pin;
-public:
-  Led(byte pin)
-  {
+class Led{
+  // setting data
+  private: // setting private data
+    byte pin;
+    public: // setting public data (so that it is accessible outside of the class)
+    Led() {} // do not use
 
+    Led(byte pin){
+      this->pin = pin;
+    }
+
+    // methods
+    void init(){
+      pinMode(pin, OUTPUT);
+    }
+
+  void init(byte defaultState){
+    init();
+    if (defaultState == HIGH) {
+      on();
+    }
+    else {
+      off();
+    }
+  }
+
+  void on(){
+    digitalWrite(pin, HIGH);
+  }
+
+  void off(){
+    digitalWrite(pin, LOW);
   }
 };
 
+Led led(boardLED); // <class> <object>([parameters/data])
+
 void setup(){
   Serial.begin(9600);
-  pinMode(LED_PIN, OUTPUT);
+  led.init();
 }
 
 void loop(){
-  digitalWrite(LED_PIN, HIGH);
+  led.on();
   delay(500);
-  digitalWrite(LED_PIN, LOW);
+  led.off();
   delay(500);
 }
