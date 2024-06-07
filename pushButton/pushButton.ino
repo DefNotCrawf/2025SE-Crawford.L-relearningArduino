1a3347b856a2dccc7bff6c6ae74dbe0cc9742f0f
Crawford L <110504159+DefNotCrawf@users.noreply.github.com>
2024-06-07 14:14:03 +1000

    PUSHBUTTON

--- pushButton/pushButton.ino
+++ pushButton/pushButton.ino
@@ -1,7 +1,15 @@
-void setup(){
+#include "PushButton.h"
 
+#define BUTTON_PIN 7
+
+PushButton button(BUTTON_PIN, true, true);
+
+void setup() {
+  Serial.begin(115200);
+  button.init();
 }
 
-void loop(){
-
+void loop() {
+  Serial.println(button.readState());
+  // delay(100);
 }
